279f0ff14e4454daa2a993c5ace50616a36ecbdd
Olena-Ignatenko <ignatenkoa2103@gmail.com>
2024-03-03 01:42:45 +0200

    Update Option.jsx

--- src/Components/Options.jsx
+++ src/Components/Options.jsx
@@ -2,9 +2,9 @@ const Options = ({ updateFeedback, totalFeedback, resetFeedback }) => {
   return (
     <div className="container-options">
       <h2>Leave Your Feedback</h2>
-      <button onClick={() => updateFeedback("good")}>Good</button>
-      <button onClick={() => updateFeedback("neutral")}>Neutral</button>
-      <button onClick={() => updateFeedback("bad")}>Bad</button>
+      <button onClick={() => updateFeedback("good")}>Good 👍</button>
+      <button onClick={() => updateFeedback("neutral")}>Neutral 😐</button>
+      <button onClick={() => updateFeedback("bad")}>Bad 👎</button>
 
       {totalFeedback > 0 && <button onClick={resetFeedback}>Reset</button>}
     </div>
